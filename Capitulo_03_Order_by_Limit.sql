52c2d5680d1ce5f1cb7cd57c3fb32be9220b3bff
Federico Tamborini <Federicotamborini01@gmail.com>
2026-04-10 18:48:33 -0300

    Capitulo_03_Order_by_Limit.sql

--- Capitulo_03_Order_by_Limit.sql
+++ Capitulo_03_Order_by_Limit.sql
@@ -29,3 +29,34 @@ SELECT * FROM payment
 WHERE amount != 0.00
 ORDER BY payment_date
 LIMIT 5
+--Ejercicios
+  
+--Caso 1: Queremos recompensar a nuestros primeros 10 clientes (ID más bajos).
+-- Uso ORDER BY para asegurar que empiezo desde el 1 y LIMIT para cortar en 10.
+SELECT first_name, last_name, email FROM customer
+ORDER BY customer_id ASC
+LIMIT 10;
+
+
+-- CASO 2: Un cliente quiere ver algo corto. ¿Cuáles son las 5 películas más breves?
+-- Ordeno por duración (length) y pido los primeros 5 resultados.
+SELECT title, length FROM film
+ORDER BY length ASC
+LIMIT 5;
+
+
+-- CASO 3: ¿Cuántas películas duran 50 minutos o menos?
+-- Mi enfoque: Ver los títulos y la duración para tener el detalle.
+SELECT title, length FROM film
+WHERE length <= 50 
+ORDER BY length;
+
+-- Enfoque de reporte: Contar el total de opciones disponibles.
+SELECT COUNT(title) FROM film
+WHERE length <= 50;
+
+/* 
+   NOTA:
+   Para el bonus, instintivamente combiné WHERE (filtro) con ORDER BY (organización).
+   esta consulta permite dar la respuesta con el detalle de los títulos, lo cual es más útil para el cliente final.
+*/
